7485f30e127ba66182746b7ec665fd199fbbd8e4
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-03-12 23:50:10 -0800

    move

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-// pub mod prime_gap;
-// pub mod quarter_of_year;
-// pub mod reverse_words;
-// pub mod summy;
-// pub mod which_are_in;
+pub mod prime_gap;
+pub mod quarter_of_year;
+pub mod reverse_words;
+pub mod summy;
+pub mod which_are_in;
